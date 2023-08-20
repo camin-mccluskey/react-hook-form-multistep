dcc1a4fc893f392006c7236aabd3f5ef0a016c01
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2023-08-20 16:19:23 +0100

    remove ts-ignore line on handleStepSubmit

--- src/multiStepForm/MultiStepFormStep.tsx
+++ src/multiStepForm/MultiStepFormStep.tsx
@@ -20,10 +20,6 @@ export default function MultiStepFormStep<StepFormData extends FieldValues>({
     ? // render step's child form
       renderStepForm({
         reportStepValidity,
-        // React does not allow for generic context types, so we can't specific the data passed to the onSubmit function inside handleStepSubmit is of type StepFormData
-        //  this is not really an issue as the onSubmit function can only be called when the form is valid, so the data passed to it will always be of type StepFormData
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
         handleStepSubmit,
       })
     : null;
